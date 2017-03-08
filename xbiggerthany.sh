0e09fc28b351c6fc8f6a9e8e9d89d9448703732e
Michael "Wootini" Maher <mikewootini@gmail.com>
2017-03-08 01:48:15 -0800

    Create xbiggerthany.sh

---
+++ xbiggerthany.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+# Is X greather than Y? 
+
+read X
+read Y
+
+if [ "$X" -gt "$Y" ]; then
+    echo "X is greater than Y";
+fi
+if [ "$X" -lt "$Y" ]; then
+    echo "X is less than Y";
+fi
+if [ "$X" -eq "$Y" ]; then
+    echo "X is equal to Y";
+fi
